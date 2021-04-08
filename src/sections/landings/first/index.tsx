bde71f9ba50d7c584fd72715305c8f74c476de30
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-04-08 15:22:33 -0400

    landings 1 & 2 (layout)

---
+++ src/sections/landings/first/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react"
+import Form from "../../../components/Form_component"
+import Landing from "../../../components/landing_component"
+import "../style.css"
+
+const FLanding: FC = () => {
+  return (
+    <Landing>
+        <>
+          <div className="f-grid">
+            <h1>
+                All your files in one secure location, accessible anywhere.
+            </h1>
+            <p>                
+                Fylo stores your most important files in one secure location. 
+                Access them wherever you need, share and collaborate with friends, 
+                family, and co-workers.
+            </p>
+            <Form placeholder="Enter your email..." buttonText="Get Started" direction="row" />
+          </div>
+
+          <div className="s-grid">
+              <img width="100%" src="/images/illustration-1.svg" alt="" />
+          </div>
+        </>
+  </Landing>
+  )
+}
+
+export default FLanding
---
+++ src/sections/landings/second/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+import Landing from "../../../components/landing_component"
+import "../style.css"
+
+const SLanding: FC = () => {
+  return (
+    <Landing>
+      <>
+        <div className="f-grid">
+          some3
+        </div>
+        <div className="s-grid">
+          some4
+        </div>
+      </>
+    </Landing>
+  )
+}
+
+export default SLanding
