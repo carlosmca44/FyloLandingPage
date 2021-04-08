081443937b2f3b85099299105d1b12b90f75735b
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-04-08 15:24:01 -0400

    add properties to landing_component

--- src/components/landing_component/index.tsx
+++ src/components/landing_component/index.tsx
@@ -2,15 +2,18 @@ import { FC } from "react"
 import "./style.css"
 
 type LandingProps = {
-    background?: string,
+    backgroundI?: string,
+    backgroundC?: string,
     children?: React.ReactChild,
 }
 
 const Landing: FC<LandingProps> = ({
+    backgroundI,
+    backgroundC,
     children,
 }) => {
   return (
-        <div className="container">
+        <div style={{backgroundImage: `url("${backgroundI}")`, backgroundRepeat: "no-repeat", backgroundColor: backgroundC}} className="container">
             {children}
         </div>
   )
