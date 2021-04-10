839d8434f6f405578d6fed5f2c1e244c3fe840e7
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-04-10 19:52:44 -0400

    second landing

--- src/sections/landings/first/index.tsx
+++ src/sections/landings/first/index.tsx
@@ -7,7 +7,7 @@ const FLanding: FC = () => {
   return (
     <Landing>
         <>
-          <div className="text-landing">
+          <div className="text-landing-first">
             <h1>
                 All your files in one secure location, accessible anywhere.
             </h1>
@@ -19,7 +19,7 @@ const FLanding: FC = () => {
             <Form placeholder="Enter your email..." buttonText="Get Started" direction="row" />
           </div>
 
-          <div>
+          <div className="image-landing">
               <img width="100%" src="/images/illustration-1.svg" alt="" />
           </div>
         </>
--- src/sections/landings/second/index.tsx
+++ src/sections/landings/second/index.tsx
@@ -1,23 +1,44 @@
 import { FC } from "react"
 import Landing from "../../../components/Landing_component"
+import PresentationCard from "../../../components/PresentationCard_component"
 import "../style.css"
 
 const SLanding: FC = () => {
   return (
     <>
       <img 
-        style={{marginBottom: -15}} 
+        style={{marginBottom: -15, marginTop: "50px"}} 
         src={ window.screen.width < 376 
           ? "/images/bg-curve-mobile.svg" 
           : "/images/bg-curve-desktop.svg" } 
           alt="" />
       <Landing backgroundC="hsl(240, 75%, 98%)">
         <>
-          <div>
-            some3
+          <div className="text-landing-second">
+            <h1>              
+              Stay productive, wherever you are
+            </h1>
+            <div className="tls-description">
+              <p>
+                Never let location be an issue when accessing your files. Fylo has you 
+                covered for all of your file storage needs.
+              </p>
+              <p>
+                Securely share files and folders with friends, family and colleagues for 
+                live collaboration. No email attachments required!
+              </p>
+            </div>
+            <div className="green-link-container">
+              <a className="green-link" href="/">
+                See how Fylo works
+                <img src="/images/icon-arrow.svg" alt="" />
+              </a>
+              <div className="green-line"></div>
+            </div>
+            <PresentationCard />
           </div>
-          <div>
-            some4
+          <div className="image-landing">
+            <img width="100%" src="/images/illustration-2.svg" alt="" />
           </div>
         </>
       </Landing>
